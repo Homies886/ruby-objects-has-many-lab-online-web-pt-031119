e6ce6e3e9df5767a4e5f7d321286b757a1f7a0f4
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-09 01:25:49 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,8 +19,8 @@ class Author
     @@post_count += 1
   end
   
-  def add_post_by_name(title)
-    post = Post.new(title)
+  def add_post_by_title(name)
+    post = Post.new(name)
     @posts << post
     post.author = self
     @@post_count += 1
