31c1c037addceebe6a4dd9640ad769f530c46025
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-09 01:22:37 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+  
+  attr_accessor :name, :author
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def author_name
+    self.author.name if author
+  end
+  
+end
