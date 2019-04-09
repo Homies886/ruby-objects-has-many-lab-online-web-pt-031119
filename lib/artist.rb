f63c93ffbe92086f2e1a40513d1e9d874a3a594d
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-09 00:32:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,15 +7,14 @@ class Artist
     @songs = []
   end
   
-  def name
-    @name
-  end
-  
   def songs
     @songs
   end
   
   def add_song(song)
+    @songs << song
+    song.artist = self
+    
     
   
 end
